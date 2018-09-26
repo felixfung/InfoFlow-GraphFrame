d231e57bff1ffb4a8a3b3760ca07e2ed4d902a39
Felix Fung <fylixeoi@gmail.com>
2018-09-25 22:39:30 -0400

    Renamed sqlContext to ss in ParquetReader

--- src/main/scala/parquetreader.scala
+++ src/main/scala/parquetreader.scala
@@ -3,14 +3,14 @@ import org.graphframes._
 
 object ParquetReader
 {
-  def apply( sqlContext: SQLContext, filename: String ): GraphFrame = {
+  def apply( ss: SparkSession, filename: String ): GraphFrame = {
     val jsonReader = new JsonReader(filename)
 
     val verticesFile = jsonReader.getVal("Vertex File").toString
     val edgesFile = jsonReader.getVal("Edge File").toString
 
-    val vertices = sqlContext.read.format("parquet").load(verticesFile)
-    val edges = sqlContext.read.format("parquet").load(edgesFile)
+    val vertices = ss.read.format("parquet").load(verticesFile)
+    val edges = ss.read.format("parquet").load(edgesFile)
 
     GraphFrame( vertices, edges )
   }
